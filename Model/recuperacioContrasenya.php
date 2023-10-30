9ce21169937a1a071b3254fb27ab1959b899bf58
David Buesa <d.buesa@sapalomera.cat>
2023-10-30 20:24:48 +0100

    dd

--- Model/recuperacioContrasenya.php
+++ Model/recuperacioContrasenya.php
@@ -1,15 +1,11 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-
-require './PHPMailer-master/src/Exception.php';             
-require './PHPMailer-master/src/PHPMailer.php';
-require './PHPMailer-master/src/SMTP.php';
-
-
+/**
+ * comprovarMailExisteix comprova si el mail existeix a la base de dades
+ *
+ * @param  mixed $mail mail a comprovar
+ * @return boolean retorna true si existeix i false si no
+ */
 function comprovarMailExisteix($mail) {
     require_once 'connexio.php';
     $stmt = $conn->prepare("SELECT * FROM usuaris WHERE mail = ?");
@@ -18,15 +14,10 @@ function comprovarMailExisteix($mail) {
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
     if($result){
         return false;
-    }
-    } catch (Exception $e) {
-        
+    }else{
+        return true;
     }
 }
-
-
-
-
 
 
 ?>
