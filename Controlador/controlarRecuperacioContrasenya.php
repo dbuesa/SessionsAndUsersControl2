13fa817a52a324d432e537eafbb9ffa7f9d69432
David Buesa <d.buesa@sapalomera.cat>
2023-10-30 20:27:41 +0100

    ff

--- Controlador/controlarRecuperacioContrasenya.php
+++ Controlador/controlarRecuperacioContrasenya.php
@@ -5,12 +5,15 @@ if(!empty($_POST['mail'])){
     require_once '../Model/utils.php';
     require_once '../Model/recuperacioContrasenya.php';
    $mail = netejarData($_POST['mail']);
-   if(!comprovarMailExisteix($mail)){
+   if(comprovarMailExisteix($mail)){
        $errors[] = "El correu no existeix";
     }
 }
 
-if(isset($_POST['recuperar'])){
+if(isset($_POST['recuperar']) && empty($errors)){
+    require_once '../Model/recuperarContrasenya.php';
+    enviarMail($mail);
+    $errors[] = "Mail enviat";
     
 }
 
